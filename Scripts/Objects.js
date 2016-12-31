fba584da7aecd1c4bb131aa2282025a425481d8c
mrhuttman <mrhuttman@gmail.com>
2016-12-31 16:39:26 -0500

    Add project files.

---
+++ Scripts/Objects.js
@@ -0,0 +1,43 @@
+﻿function Music_CD(itemNo, artist, title, imageUrl_sm, imageUrl_lg,
+    isMixed, binder, numDiscs, misc)
+{
+    this.itemNo_pk = itemNo;    // Note: These fields must exactly match (case-sensitive) the schema of the WCF objects
+    this.Artist = artist;
+    this.Title = title;
+    this.imageUrl_sm = imageUrl_sm;
+    this.imageUrl_lg = imageUrl_lg;
+    this.isMixed = isMixed;
+    this.binder = binder;
+    this.numDiscs = numDiscs;
+    this.misc = misc;
+}
+
+function Empty_Music_CD() {
+    return new Music_CD(0, "", "", "", "", false, 0, 0, "");
+}
+
+function SearchCDs_Binder_Input(binderNo, page) {
+    this.BinderNo = binderNo;
+    this.Page = page;
+}
+
+function SearchCDs_Artist_Input(artist, page) {
+    this.Artist = artist;
+    this.Page = page;
+}
+
+function SearchCDs_Title_Input(title, page) {
+    this.Title = title;
+    this.Page = page;
+}
+
+function SearchCDs_Advanced_Input(page, filter_isMixed, searchCD)
+{
+    this.Page = page;
+    this.Filter_isMixed = filter_isMixed;
+    this.SearchCD = searchCD;
+}
+
+function Empty_SearchCDs_Advanced_Input() {
+    return new SearchCDs_Advanced_Input(-1, false, new Empty_Music_CD());
+}
---
+++ Scripts/Utility.js
@@ -0,0 +1,38 @@
+﻿// AJAX helper function - calls WCF web service
+function CDServiceAJAX(type, method, data, successCallback, errorCallback)
+{
+    $.ajax({
+        type: type,
+        url: 'http://localhost:64246/Media_DB/CDServiceWCF.svc/' + method + '?callback=?',
+        contentType: 'application/json; charset=utf-8',        
+        data: data,
+        dataType: 'jsonp',
+        success: function (result) {            
+            if ('function' === typeof successCallback)
+            {
+                successCallback(result);
+            }            
+        },
+        error: function (xhr, status, error) {
+            console.log(error);
+            if ('function' === typeof errorCallback)
+            {
+                errorCallback(error);
+            }
+        }
+    });
+}
+
+// Hide Bootstrap alerts without removing them from the DOM
+// http://jsfiddle.net/cQNFL/21/
+$("[data-hide]").on("click", function ()
+{
+    $(this).closest("." + $(this).attr("data-hide")).addClass('hidden');
+});
+
+// Get parameter from a URL - used on edit and search results pages
+// http://stackoverflow.com/questions/11582512/how-to-get-url-parameters-with-javascript/11582513#11582513
+function getURLParameter(name)
+{
+    return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+}
